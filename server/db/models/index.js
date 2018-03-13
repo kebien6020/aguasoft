adff4bbac4b6f6497e330bcc24d1a16865ad12d1
Kevin Peña <kevin.pena.prog@gmail.com>
2018-03-12 19:44:45 -0500

    DB code is Javascript

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,8 +1,8 @@
 // This file is called index so you can require the whole folder
-import * as fs from 'fs'
-import * as path from 'path'
-import * as makeDegub from 'debug'
-import * as Sequelize from 'sequelize'
+const fs = require('fs')
+const path = require('path')
+const makeDegub = require('debug')
+const Sequelize = require('sequelize')
 const debug = makeDegub('app:db')
 
 const thisFile = path.basename(module.filename)
@@ -10,14 +10,6 @@ const thisFile = path.basename(module.filename)
 const env = process.env.NODE_ENV || 'development'
 // load the config for this environment
 const config = require(path.resolve(__dirname, '../config.json'))[env]
-// Here will be placed all the model classes
-interface DBExport {
-  sequelize: Sequelize.Sequelize
-  Sequelize: Sequelize.SequelizeStatic
-  [idx: string]: Sequelize.Model<any, any> |
-                 Sequelize.Sequelize |
-                 Sequelize.SequelizeStatic
-}
 
 // Log database connection
 debug(`Using ${config.dialect} database, in storage ${config.storage}`)
@@ -26,8 +18,9 @@ config.logging = require('debug')('app:sql')
 // Connect
 const sequelize = new Sequelize(config.database, config.username, config.password, config)
 
+// Here will be placed all the model classes
 // Add sequelize instance and class to the exports
-const db: DBExport = {
+const db = {
   sequelize: sequelize,
   Sequelize: Sequelize
 }
@@ -36,22 +29,22 @@ fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== thisFile) && (file.slice(-3) === '.js'))
   .forEach(function(file) {
-      // Models have to be in a specific way for them to be
-      // able to be imported by Sequelize.prototype.import
+    // Models have to be in a specific way for them to be
+    // able to be imported by Sequelize.prototype.import
     const model = sequelize.import(path.join(__dirname, file))
-      // Log all added models
+    // Log all added models
     debug(`adding ${model.name} to models`)
-      // Actually add them to the db object
+    // Actually add them to the db object
     db[model.name] = model
   })
 
 // Setup model associations
 Object.keys(db).forEach(modelName => {
-  const model = db[modelName] as Sequelize.Model<any, any>
+  const model = db[modelName]
   if (model.associate) {
     debug(`setting up ${modelName} associations`)
-    model.associate(db as Sequelize.Models)
+    model.associate(db)
   }
 })
 
-export default db
+module.exports = db
