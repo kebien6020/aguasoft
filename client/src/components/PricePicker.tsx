40d89465c0a4a8ba4eafa40e4df40b5b24ba5884
Kevin Peña <kevin.pena.prog@gmail.com>
2018-12-24 16:18:21 -0500

    [Client] Add 'Add Price' Button to PricePicker

--- client/src/components/PricePicker.tsx
+++ client/src/components/PricePicker.tsx
@@ -1,11 +1,48 @@
 import * as React from 'react'
 
-class PricePicker extends React.Component {
+import { StyleRulesCallback, Theme, withStyles } from '@material-ui/core/styles'
+
+import Button from '@material-ui/core/Button'
+import AddIcon from '@material-ui/icons/AddCircle'
+
+interface Props extends PropClasses {
+
+}
+
+class PricePicker extends React.Component<Props> {
   render() {
+    const { props } = this
+    const { classes } = props
     return (
-      <p>Hit</p>
+      <>
+        <Button
+          className={classes.button}
+          variant='outlined'
+          color='primary'
+        >
+          <AddIcon className={[classes.icon, classes.leftIcon].join(' ')} />
+          Agregar Precio
+        </Button>
+      </>
     )
   }
 }
 
-export default PricePicker
+const styles : StyleRulesCallback = (theme: Theme) => ({
+  button: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'block',
+    '& *': {
+      verticalAlign: 'top',
+    },
+  },
+  icon: {
+    fontSize: '1.5em',
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+})
+
+export default withStyles(styles)(PricePicker)
