a7f9fd745b14a592be3121b22467c21e8aadf940
Kevin Peña <kevin.pena.prog@gmail.com>
2019-01-22 15:39:02 -0500

    [Server] Add constraint so that client name and code aren't empty

---
+++ server/src/db/migrations/20190122201816-add-client-noempty-constraints.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const CODE_CK = 'Prices_code_noempty'
+const NAME_CK = 'Prices_name_noempty'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const { ne } = Sequelize.Op
+    const commonOptions = {
+      type: 'check',
+      logging: console.log,
+    }
+
+    const checkNoEmptyCode = Object.assign({}, commonOptions, {
+      name: CODE_CK,
+      where: {
+        code: { [ne]: '' },
+      },
+    })
+
+    const checkNoEmptyName = Object.assign({}, commonOptions, {
+      name: NAME_CK,
+      where: {
+        name: { [ne]: '' },
+      },
+    })
+
+    const sequelize = queryInterface.sequelize
+    return sequelize.transaction(t =>
+      queryInterface.addConstraint('Clients', ['code'], checkNoEmptyCode).then(() =>
+        queryInterface.addConstraint('Clients', ['name'], checkNoEmptyName)
+      )
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const sequelize = queryInterface.sequelize
+
+    return sequelize.transaction(t =>
+      queryInterface.removeConstraint('Clients', NAME_CK).then(() =>
+        queryInterface.removeConstraint('Clients', CODE_CK)
+      )
+    )
+  }
+};
