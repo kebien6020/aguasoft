d2e1955113a8dd056cbeb0ce997a56edb19a6855
Kevin Peña <kevin.pena.prog@gmail.com>
2019-03-06 20:54:14 -0500

    [Client] Switch colors for code 002 and 003

--- client/src/components/Sells.tsx
+++ client/src/components/Sells.tsx
@@ -117,8 +117,8 @@ class Sells extends React.Component<SellsPropsAll, SellsState> {
 
     const userColorLookup : {[index:string] : string} = {
       '001': colors.blue[500],
-      '002': colors.green[500],
-      '003': colors.pink[500],
+      '002': colors.pink[500],
+      '003': colors.green[500],
     }
 
     const getUserColor = (userCode: string) => (
