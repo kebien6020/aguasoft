1c283c77b0a59ba80b1a8e5904d14e7b896cfad1
Kevin Peña <kevin.pena.prog@gmail.com>
2019-03-16 16:58:18 -0500

    [Client] Extract common Title component

---
+++ client/src/components/Title.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+import {
+  Typography
+} from '@material-ui/core'
+
+import {
+  withStyles,
+  StyleRules,
+} from '@material-ui/core/styles'
+
+interface Props extends PropClasses {
+  children: any
+  className?: string
+}
+
+const Title = (props: Props) => (
+  <div className={[props.classes.title, props.className].join(' ')}>
+    <Typography variant='h6'>{props.children}</Typography>
+  </div>
+)
+
+const styles: StyleRules = {
+  title: {
+    '& > *': {
+      textAlign: 'center',
+    },
+  },
+}
+
+export default withStyles(styles)(Title)
