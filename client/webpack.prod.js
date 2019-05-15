b108b9757d6a70e234c2567c8979d297844616de
Kevin Peña <kevin.pena.prog@gmail.com>
2019-05-14 23:13:26 -0500

    [Client] For production use by default only hashes in filenames

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,10 +1,12 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
+const detailedNames = process.env.DETAILED_NAMES || false
+
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    filename: '[name]-[hash].js',
-    chunkFilename: '[name]-[chunkhash].js',
+    filename: (detailedNames ? '[name]-' : '') + '[hash].js',
+    chunkFilename: (detailedNames ? '[name]-' : '') + '[chunkhash].js',
   }
 })
