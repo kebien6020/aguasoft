f5213d4acd8ed358eee49aa8391338abb0efcdf1
Kevin Peña <kevin.pena.prog@gmail.com>
2019-05-19 12:02:47 -0500

    [Server] Add Spendings migration and model

---
+++ server/src/db/migrations/20190519161153-create-spendings.js
@@ -0,0 +1,55 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Spendings', {
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      fromCash: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      isTransfer: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'restrict',
+        onUpdate: 'restrict',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      },
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Spendings')
+  }
+}
---
+++ server/src/db/models/spendings.ts
@@ -0,0 +1,78 @@
+import { Sequelize, DataTypes, Model } from 'sequelize'
+import { ModelStatic } from '../type-utils'
+import { User } from './users'
+import {
+  BelongsToGetAssociationMixin,
+  BelongsToSetAssociationMixin,
+} from 'sequelize'
+
+export interface Spending extends Model {
+  // The date the spending was performed
+  readonly date: Date
+
+  readonly description: string
+
+  readonly value: string
+  // Whether this was paid out of cash
+  readonly fromCash: boolean
+  // Whether this is a transfer to our bank account
+  readonly isTransfer: boolean
+  // timestamps
+  readonly createdAt: Date
+  readonly updatedAt: Date
+  readonly deletedAt: Date | null
+
+  // Possible inclusions
+  readonly user?: User
+
+  // Methods added when performing associations
+  getUser: BelongsToGetAssociationMixin<User>
+  setUser: BelongsToSetAssociationMixin<User, number>
+}
+
+export type SpendingStatic = ModelStatic<Spending>
+
+export default function(sequelize: Sequelize) {
+  const Spendings = <SpendingStatic> sequelize.define('Spendings', {
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    value: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    fromCash: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    isTransfer: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onDelete: 'restrict',
+      onUpdate: 'restrict',
+    },
+  }, {
+    paranoid: true,
+  })
+
+  Spendings.associate = function(models) {
+    Spendings.belongsTo(models.Users)
+  }
+
+  return Spendings
+}
