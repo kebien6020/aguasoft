aeeecd8e04a46acd1690352ebf9f8f5f83559c24
Kevin Peña <kevin.pena.prog@gmail.com>
2019-05-18 20:14:13 -0500

    [Client] Actually show payments in PaymentList, paginated

--- client/src/Routes/PaymentList.tsx
+++ client/src/Routes/PaymentList.tsx
@@ -11,20 +11,115 @@ import BackIcon from '@material-ui/icons/ArrowBack'
 import { AuthRouteComponentProps } from '../AuthRoute'
 import adminOnly from '../hoc/adminOnly'
 import Layout from '../components/Layout'
+import LoadingScreen from '../components/LoadingScreen'
+import Payments from '../components/Payments'
+import ResponsiveContainer from '../components/ResponsiveContainer'
+import { Payment } from '../models'
+import { fetchJsonAuth, ErrorResponse, SuccessResponse, isErrorResponse } from '../utils'
+
+import Pagination from 'material-ui-flat-pagination'
+import * as moment from 'moment'
+
+interface PaymentPageResponse {
+  payments: Payment[]
+  totalCount: number
+}
 
 type Props = AuthRouteComponentProps<any> & PropClasses
 
 interface State {
-
+  payments: Payment[] | null
+  totalCount: number
+  offset: number
+  disablePagination: boolean
 }
+
+const ITEMS_PER_PAGE = 30
 
 class PaymentList extends React.Component<Props, State> {
 
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      payments: null,
+      totalCount: 0,
+      offset: 0,
+      disablePagination: false
+    }
+  }
+
+  componentDidMount() {
+    this.updatePayments(this.state.offset)
+  }
+
+  updatePayments = async (offset: number) => {
+    const { props } = this
+    const res: ErrorResponse | PaymentPageResponse = await fetchJsonAuth(
+      `/api/payments/paginate?limit=${ITEMS_PER_PAGE}&offset=${offset}`,
+      props.auth
+    )
+
+    if (!isErrorResponse(res)) {
+      const { payments, totalCount } = res
+      this.setState({payments, totalCount})
+    } else {
+      console.error(res.error)
+    }
+  }
+
+  handleDeletePayment = async (paymentId: number) => {
+    if (!this.state.payments) return
+
+    const { props } = this
+
+    const result : ErrorResponse | SuccessResponse = await
+      fetchJsonAuth('/api/payments/' + paymentId, props.auth, {
+        method: 'delete',
+      })
+
+    if (!isErrorResponse(result)) {
+      const payments = [...this.state.payments]
+      const payment = payments.find(p => p.id === paymentId)
+
+      if (!payment) {
+        console.error('Trying to mutate unknown paymentId', paymentId)
+        return
+      }
+
+      payment.deletedAt = moment().toISOString()
+
+      this.setState({payments})
+    } else {
+      console.error(result.error)
+    }
+  }
+
+  handlePageChange = async (_event: any, offset: number) => {
+    this.setState({disablePagination: true})
+    await this.updatePayments(offset)
+    this.setState({offset, disablePagination: false})
+  }
+
   renderLinkBack = (props: any) => <Link to='/' {...props} />
 
+  renderPagination = () => (
+    <Pagination
+      limit={ITEMS_PER_PAGE}
+      offset={this.state.offset}
+      total={this.state.totalCount}
+      onClick={this.handlePageChange}
+      disabled={this.state.disablePagination}
+      className={this.props.classes.pagination}
+    />
+  )
+
   render() {
-    const { props } = this
+    const { props, state } = this
     const { classes } = props
+
+    if (state.payments === null) {
+      return <LoadingScreen text='Cargando pagos...' />
+    }
 
     return (
       <Layout>
@@ -43,6 +138,14 @@ class PaymentList extends React.Component<Props, State> {
             </Typography>
           </Toolbar>
         </AppBar>
+        <ResponsiveContainer>
+          {this.renderPagination()}
+          <Payments
+            payments={state.payments}
+            onDeletePayment={this.handleDeletePayment}
+          />
+          {this.renderPagination()}
+        </ResponsiveContainer>
       </Layout>
     )
   }
@@ -63,6 +166,9 @@ const styles : StyleRulesCallback = (_theme: Theme) => ({
       fontWeight: 400,
     },
   },
+  pagination: {
+    textAlign: 'center',
+  }
 })
 
 export default
