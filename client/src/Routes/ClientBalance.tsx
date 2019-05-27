aed04664201d0e565079d7a4b0c7f179fe2d7c80
Kevin Peña <kevin.pena.prog@gmail.com>
2019-05-26 19:24:14 -0500

    [Client] Show balance history for a client in ClientBalance

--- client/src/Routes/ClientBalance.tsx
+++ client/src/Routes/ClientBalance.tsx
@@ -1,25 +1,242 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 import { withStyles, Theme, StyleRulesCallback } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Card from '@material-ui/core/Card'
+import IconButton from '@material-ui/core/IconButton'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import BackIcon from '@material-ui/icons/ArrowBack'
+import * as colors from '@material-ui/core/colors'
 
 import { AuthRouteComponentProps } from '../AuthRoute'
 import adminOnly from '../hoc/adminOnly'
+import Layout from '../components/Layout'
+import LoadingScreen from '../components/LoadingScreen'
+import ResponsiveContainer from '../components/ResponsiveContainer'
+import { fetchJsonAuth, ErrorResponse, isErrorResponse, money } from '../utils'
 
-type Props = AuthRouteComponentProps<any> & PropClasses
+import { Moment } from 'moment'
+import * as moment from 'moment'
 
-interface State {
-
+interface Params {
+  id: string
 }
 
+type Props = AuthRouteComponentProps<Params> & PropClasses
+
+interface State {
+  changeGroups: ChangeGroup[] | null
+}
+
+interface Change {
+  date: string
+  value: number
+  type: 'sell' | 'payment'
+  id: number
+}
+
+interface IncompleteChangeGroup {
+  date: Moment
+  total: number
+  changes: Change[]
+  type: 'sell' | 'payment'
+  newBalance?: number
+}
+
+interface ChangeGroup extends IncompleteChangeGroup {
+  newBalance: number
+}
+
+type ChangesResponse = { changes: Change[] } | ErrorResponse
+
 class ClientBalance extends React.Component<Props, State> {
-  render() {
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      changeGroups: null,
+    }
+  }
+
+  groupChanges = (changes: Change[]) => {
+    type Groups = IncompleteChangeGroup[]
+
+    const pushNewGroup = (change: Change, groups: Groups) => {
+      groups.push({
+        date: moment(change.date).startOf('day'),
+        total: change.value,
+        changes: [change],
+        type: change.type
+      })
+    }
+
+    const addToLastGroup = (change: Change, prev: IncompleteChangeGroup) => {
+      prev.total += change.value
+      prev.changes.push(change)
+    }
+
+    const groups : Groups = []
+
+    for (const change of changes) {
+      const prev = groups[groups.length - 1]
+
+      // New group when:
+      const newGroup =
+        // no previous
+        prev === undefined ||
+        // do not mix types
+        prev.type !== change.type ||
+        // do not combine payments
+        prev.type === 'payment' ||
+        // only combine from same day
+        !prev.date.isSame(change.date, 'day')
+
+      if (newGroup) {
+        pushNewGroup(change, groups)
+      } else {
+        addToLastGroup(change, prev)
+      }
+    }
+
+    for (let i = 0; i < groups.length; ++i) {
+      const prev = groups[i - 1]
+
+      let balance = prev === undefined ? 0 : prev.newBalance as number
+      const chngValue = groups[i].total
+      const delta = groups[i].type === 'sell' ? chngValue : -chngValue
+      balance += delta
+      groups[i].newBalance = balance
+    }
+
+    groups.reverse()
+
+    return groups as ChangeGroup[]
+  }
+
+  async componentDidMount() {
+    const { props } = this
+    const { params } = props.match
+    const clientId = params.id
+
+    const response : ChangesResponse =
+      await fetchJsonAuth(`/api/clients/${clientId}/balance`, props.auth)
+
+    if (!isErrorResponse(response)) {
+      const changeGroups = this.groupChanges(response.changes)
+      this.setState({changeGroups})
+    } else {
+      console.error(response.error)
+    }
+  }
+
+  renderLinkBack = (props: any) => <Link to='/clients' {...props} />
+
+  renderChangeGroup = (ch: ChangeGroup, key: React.Key) => {
+    const { classes } = this.props
     return (
-      <p>Hit</p>
+      <Card className={classes.card} key={key}>
+        <div className={classes.cardDate}>
+          <Typography>{ch.date.format("DD/MMM/YY")}</Typography>
+        </div>
+        <div className={classes.cardType}>
+          <Typography className={ch.type === 'sell' ? classes.sell : classes.payment}>
+            {ch.type === 'sell' ? 'Venta' : 'Pago'}
+          </Typography>
+        </div>
+        <div className={classes.cardValue}>
+          <Typography>
+            {ch.type === 'sell' ?
+              'Ventas del día:' :
+              'Valor pagado: '
+            }
+            {money(ch.total)}
+          </Typography>
+        </div>
+        <div className={classes.cardBalance}>
+          <Typography>
+            Balance: {money(ch.newBalance)}
+          </Typography>
+        </div>
+      </Card>
+    )
+  }
+
+  render() {
+    const { props, state } = this
+    const { classes } = props
+
+    if (state.changeGroups === null) {
+      return <LoadingScreen text='Cargando balance...' />
+    }
+
+    return (
+      <Layout>
+        <AppBar position='static' className={classes.appbar}>
+          <Toolbar>
+            <IconButton
+              className={classes.backButton}
+              color='inherit'
+              aria-label='Back'
+              component={this.renderLinkBack}
+            >
+              <BackIcon />
+            </IconButton>
+            <Typography variant='h6' color='inherit' className={classes.title}>
+              Balance Cliente:
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <ResponsiveContainer>
+          <div className={classes.cardContainer}>
+            {state.changeGroups.map(this.renderChangeGroup)}
+          </div>
+        </ResponsiveContainer>
+      </Layout>
     )
   }
 }
 
-const styles : StyleRulesCallback = (_theme: Theme) => ({
+const styles : StyleRulesCallback = (theme: Theme) => ({
+  appbar: {
+    flexGrow: 1,
+  },
+  backButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  card: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    textAlign: 'center',
+    marginTop: theme.spacing.unit * 2,
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    '& > div': {
+      width: '15%',
+    },
+  },
+  cardValue: {
+    width: '35% !important',
+  },
+  cardBalance: {
+    width: '35% !important',
+  },
+  sell: {
+    color: colors.green[500],
+  },
+  payment: {
+    color: colors.red[500],
+  },
+  title: {
+    flexGrow: 1,
+    '& h6': {
+      fontSize: '48px',
+      fontWeight: 400,
+    },
+  },
 })
 
 export default
