ebcc579c20360a9ca23d748dd372c91bce42da07
Kevin Peña <kevin.pena.prog@gmail.com>
2019-10-13 15:57:30 -0500

    [Client] Add login section to /payments

--- client/src/Routes/Payments.tsx
+++ client/src/Routes/Payments.tsx
@@ -1,11 +1,40 @@
 import * as React from 'react'
+import { useCallback } from 'react'
+import { useHistory } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
 
+import Paper from '@material-ui/core/Paper'
+
+import { AuthRouteComponentProps } from '../AuthRoute'
 import Layout from '../components/Layout'
+import Login from '../components/Login'
+import Title from '../components/Title'
 
-export default function Payments() {
+type PaymentsProps = AuthRouteComponentProps<{}>;
+export default function Payments({auth}: PaymentsProps) {
+  const classes = useStyles()
+
+  // Login to register sell
+  const history = useHistory()
+  const handleLogin = useCallback(() => {
+    history.push('/payment')
+  }, [history])
+  const loginElem =
+    <Paper className={classes.login}>
+      <Login onSuccess={handleLogin} auth={auth} />
+    </Paper>
+
   return (
     <Layout title='Pagos'>
+      <Title>Registrar Pago</Title>
+      {loginElem}
 
     </Layout>
   )
 }
+
+const useStyles = makeStyles(theme => ({
+  login: {
+    padding: theme.spacing(2),
+  },
+}))
