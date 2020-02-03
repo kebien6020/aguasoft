d83a0b6feee43d0d4507a12332eb3b0773b8a0a5
Kevin Peña <kevin.pena.prog@gmail.com>
2020-02-02 20:09:47 -0500

    [Client] Implement unpack movement

--- client/src/Routes/RegisterUnpack.tsx
+++ client/src/Routes/RegisterUnpack.tsx
@@ -1,17 +1,72 @@
 import * as React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 
+import useAuth from '../hooks/useAuth'
+import useSnackbar from '../hooks/useSnackbar'
+import Form from '../components/form/Form'
 import Layout from '../components/Layout'
+import TextField from '../components/form/TextField'
 import Title from '../components/Title'
+import Yup from '../components/form/Yup'
+import { fetchJsonAuth, isErrorResponse } from '../utils'
+
+const initialValues = {
+  amount: '',
+}
+
+const validationSchema = Yup.object({
+  amount: Yup.number().integer().positive().required(),
+})
+
+type Values = typeof initialValues
 
 const RegisterUnpack = () => {
   const classes = useStyles()
+
+  const auth = useAuth()
+  const showMessage = useSnackbar()
+
+  const handleSubmit = async (values: Values) => {
+    const url = '/api/inventory/movements/unpack'
+    let payload: Object = {
+      amount: Number(values.amount),
+    }
+
+    const response = await fetchJsonAuth(url, auth, {
+      method: 'post',
+      body: JSON.stringify(payload)
+    })
+
+    if (isErrorResponse(response)) {
+      showMessage('Error: ' + response.error.message)
+      return
+    }
+
+    showMessage('Guardado exitoso')
+  }
 
   return (
     <Layout title='Registrar Desempaque'>
       <Paper className={classes.paper}>
         <Title>Registrar Desempaque</Title>
+
+        <Form
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+            <Grid item xs={12}>
+              <TextField
+                name='amount'
+                label='Bolsas a desempacar'
+              />
+            </Grid>
+
+            <SubmitButton />
+        </Form>
       </Paper>
     </Layout>
   )
@@ -27,5 +82,25 @@ const useStyles = makeStyles(theme => ({
     marginBottom: theme.spacing(2),
   },
 }))
+
+const SubmitButton = () => {
+  const classes = useSubmitButtonStyles()
+
+  return (
+    <Grid item xs={12}>
+      <Button variant='contained' color='primary' type='submit' className={classes.button}>
+        Registrar
+      </Button>
+    </Grid>
+  )
+}
+
+const useSubmitButtonStyles = makeStyles({
+  button: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
+})
 
 export default RegisterUnpack
