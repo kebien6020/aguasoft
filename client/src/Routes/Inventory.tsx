f0338f543c36dd1d85a5c9209e6fb59bebaa2b88
Kevin Peña <kevin.pena.prog@gmail.com>
2020-02-23 11:45:28 -0500

    [Client] Extract StorageCard to a different file

--- client/src/Routes/Inventory.tsx
+++ client/src/Routes/Inventory.tsx
@@ -2,75 +2,19 @@ import * as React from 'react'
 import { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
 import Grid from '@material-ui/core/Grid'
 import * as socketio from 'socket.io-client'
 
 import useFetch from '../hooks/useFetch'
 import useUser from '../hooks/useUser'
 import useSnackbar from '../hooks/useSnackbar'
+import useStorageStates from '../hooks/api/useStorageStates'
 import Layout from '../components/Layout'
 import LoadingIndicator from '../components/LoadingIndicator'
 import ManualMovementForm from '../components/inventory/ManualMovementForm'
 import Title from '../components/Title'
-import { Storage, InventoryElement, StorageState } from '../models'
-import useStorageStates from '../hooks/api/useStorageStates'
-
-interface StorageCardProps {
-  storage: Storage
-  storageStates: StorageState[]
-  inventoryElements: InventoryElement[]
-}
-
-const StorageCard = (props: StorageCardProps) => {
-  const classes = useStorageCardStyles()
-
-  const { storage, storageStates, inventoryElements } = props
-  const ownStorageStates = storageStates.filter(state => state.storageId === storage.id)
-  return (
-    <Card className={classes.card}>
-      <CardHeader
-        title={storage.name}
-        className={classes.header}
-        classes={{
-          title: classes.title,
-        }}
-      />
-      <CardContent>
-        {ownStorageStates.map(state => {
-          const inventoryElement = inventoryElements
-            .find(element => element.id === state.inventoryElementId)
-
-          const elemName = inventoryElement ? inventoryElement.name : 'Desconocido'
-
-          return (
-            <div key={state.inventoryElementId}>
-              <strong>{elemName}</strong>: {state.quantity}
-            </div>
-          )
-        })}
-      </CardContent>
-    </Card>
-  )
-}
-
-const useStorageCardStyles = makeStyles(theme => ({
-  card: {
-    borderLeftWidth: '4px',
-    borderLeftStyle: 'solid',
-    borderLeftColor: theme.palette.primary.main,
-    height: '100%',
-  },
-  header: {
-    borderBottom: '1px solid rgba(0,0,0,0.1)',
-  },
-  title: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-  },
-}))
+import StorageCard from '../components/inventory/StorageCard'
+import { Storage, InventoryElement } from '../models'
 
 const socket = socketio('/', {
   autoConnect: false,
