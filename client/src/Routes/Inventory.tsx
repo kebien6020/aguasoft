a37837fe0a30e06630f3fc7e5c89909d3e248247
Kevin Peña <kevin.pena.prog@gmail.com>
2020-02-23 11:52:12 -0500

    [Client] Extract CurrentInventoryState component

--- client/src/Routes/Inventory.tsx
+++ client/src/Routes/Inventory.tsx
@@ -1,106 +1,15 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import * as socketio from 'socket.io-client'
 
-import useFetch from '../hooks/useFetch'
-import useUser from '../hooks/useUser'
-import useSnackbar from '../hooks/useSnackbar'
-import useStorageStates from '../hooks/api/useStorageStates'
+import CurrentInventoryState from '../components/inventory/CurrentInventoryState'
 import Layout from '../components/Layout'
-import LoadingIndicator from '../components/LoadingIndicator'
-import ManualMovementForm from '../components/inventory/ManualMovementForm'
-import Title from '../components/Title'
-import StorageCard from '../components/inventory/StorageCard'
-import { Storage, InventoryElement } from '../models'
 
-const socket = socketio('/', {
-  autoConnect: false,
-})
-
-export default function Inventory() {
-  const classes = useStyles()
-
-  // Create manual movement
-  const { isAdmin } = useUser()
-  const [showManualMovementForm, setShowManualMovementForm] = useState(false)
-  const manualMovementButton =
-    isAdmin && (
-      <Button
-        variant='outlined'
-        color='primary'
-        onClick={() => setShowManualMovementForm(prev => !prev)}
-      >
-        Crear movimiento manual
-      </Button>
-    )
-
-  // Fetch from server
-  const showError = useSnackbar()
-  const [storages] = useFetch<Storage[]>('/api/inventory/storages', {
-    showError,
-    name: 'la lista de almacenamientos',
-  })
-  const [inventoryElements] = useFetch<InventoryElement[]>('/api/inventory/inventoryElements', {
-    showError,
-    name: 'la lista de elementos',
-  })
-
-  const [storageStates, update] = useStorageStates()
-
-  useEffect(() => {
-    socket.open()
-
-    const onChange = () => update()
-    socket.on('storageStatesChanged', onChange)
-    socket.on('reconnect', onChange)
-
-    return () => {
-      socket.off('reconnect', onChange)
-      socket.off('storageStatesChanged', onChange)
-
-      socket.close()
-    }
-  }, [])
-
+const Inventory = () => {
   return (
     <Layout title='Inventario'>
-      <Title>Grafico Principal</Title>
-      <div className={classes.button}>
-        {manualMovementButton}
-      </div>
-
-      {showManualMovementForm &&
-        <ManualMovementForm
-          storages={storages}
-          inventoryElements={inventoryElements}
-          onUpdate={update}
-        />
-      }
-
-      <Grid container spacing={2}>
-        {storages && storageStates && inventoryElements ? storages.map(storage =>
-          <Grid item key={storage.id} xs={12} md={6}>
-            <StorageCard
-              storage={storage}
-              storageStates={storageStates}
-              inventoryElements={inventoryElements}
-            />
-          </Grid>
-        ) : <LoadingIndicator />}
-      </Grid>
+      <CurrentInventoryState />
 
     </Layout>
   )
 }
 
-const useStyles = makeStyles(() => ({
-  button: {
-    display: 'flex',
-    flexFlow: 'column nowrap',
-    alignItems: 'center',
-    marginBottom: '1rem',
-  }
-}))
+export default Inventory
---
+++ client/src/components/inventory/CurrentInventoryState.tsx
@@ -0,0 +1,106 @@
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import * as socketio from 'socket.io-client'
+
+import useFetch from '../../hooks/useFetch'
+import useUser from '../../hooks/useUser'
+import useSnackbar from '../../hooks/useSnackbar'
+import useStorageStates from '../../hooks/api/useStorageStates'
+import LoadingIndicator from '../LoadingIndicator'
+import ManualMovementForm from './ManualMovementForm'
+import Title from '../Title'
+import StorageCard from './StorageCard'
+import { Storage, InventoryElement } from '../../models'
+
+const socket = socketio('/', {
+  autoConnect: false,
+})
+
+const CurrentInventoryState = () => {
+  const classes = useStyles()
+
+  // Create manual movement
+  const { isAdmin } = useUser()
+  const [showManualMovementForm, setShowManualMovementForm] = useState(false)
+  const manualMovementButton =
+    isAdmin && (
+      <Button
+        variant='outlined'
+        color='primary'
+        onClick={() => setShowManualMovementForm(prev => !prev)}
+      >
+        Crear movimiento manual
+      </Button>
+    )
+
+  // Fetch from server
+  const showError = useSnackbar()
+  const [storages] = useFetch<Storage[]>('/api/inventory/storages', {
+    showError,
+    name: 'la lista de almacenamientos',
+  })
+  const [inventoryElements] = useFetch<InventoryElement[]>('/api/inventory/inventoryElements', {
+    showError,
+    name: 'la lista de elementos',
+  })
+
+  const [storageStates, update] = useStorageStates()
+
+  useEffect(() => {
+    socket.open()
+
+    const onChange = () => update()
+    socket.on('storageStatesChanged', onChange)
+    socket.on('reconnect', onChange)
+
+    return () => {
+      socket.off('reconnect', onChange)
+      socket.off('storageStatesChanged', onChange)
+
+      socket.close()
+    }
+  }, [])
+
+  return (
+    <>
+      <Title>Grafico Principal</Title>
+      <div className={classes.button}>
+        {manualMovementButton}
+      </div>
+
+      {showManualMovementForm &&
+        <ManualMovementForm
+          storages={storages}
+          inventoryElements={inventoryElements}
+          onUpdate={update}
+        />
+      }
+
+      <Grid container spacing={2}>
+        {storages && storageStates && inventoryElements ? storages.map(storage =>
+          <Grid item key={storage.id} xs={12} md={6}>
+            <StorageCard
+              storage={storage}
+              storageStates={storageStates}
+              inventoryElements={inventoryElements}
+            />
+          </Grid>
+        ) : <LoadingIndicator />}
+      </Grid>
+    </>
+  )
+}
+
+const useStyles = makeStyles(() => ({
+  button: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    alignItems: 'center',
+    marginBottom: '1rem',
+  }
+}))
+
+export default CurrentInventoryState
