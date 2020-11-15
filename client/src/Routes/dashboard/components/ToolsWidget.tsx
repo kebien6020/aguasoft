071291cfefde68f072aeee7c490df962c641885c
Kevin Peña <kevin.pena.prog@gmail.com>
2020-11-15 15:24:53 -0500

    Add link to billing summary in dashboard

---
+++ client/src/Routes/dashboard/components/ToolsWidget.tsx
@@ -0,0 +1,25 @@
+import { List, ListItem, ListItemIcon, ListItemText, Paper } from '@material-ui/core'
+import React from 'react'
+import Title from '../../../components/Title'
+import { TableChart as TableIcon } from '@material-ui/icons'
+import { Link, LinkProps } from 'react-router-dom'
+import { MakeOptional } from '../../../utils'
+
+export const ToolsWidget = (): JSX.Element => {
+  return (<>
+    <Title>Herramientas</Title>
+    <Paper>
+      <List>
+        <ListItem button component={LinkToBillingSummary}>
+          <ListItemIcon>
+            <TableIcon />
+          </ListItemIcon>
+          <ListItemText primary='Facturación' />
+        </ListItem>
+      </List>
+    </Paper>
+  </>)
+}
+
+const LinkToBillingSummary = (props: MakeOptional<LinkProps, 'to'>) =>
+  <Link to='/tools/billing-summary' {...props} />
