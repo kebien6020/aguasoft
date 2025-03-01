561a35483a83f79d0e3f8768a9612c85a6091a93
Kevin Peña <kevin.pena.prog@gmail.com>
2025-03-01 16:05:57 -0500

    Add push script

--- alias.sh
+++ alias.sh
@@ -1,3 +1,4 @@
 alias dc='docker compose'
 alias dcp='docker compose -f docker-compose.prod.yml'
 alias a='docker compose exec server'
+alias prod-push='echo aguasoft nginx certbot | xargs -n1 docker-compose -f docker-compose.prod.yml build --push'
